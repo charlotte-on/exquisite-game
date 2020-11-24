94c22b56076340af5f0e07173cdc6512c6f7ea1c
Charlotte Oury <charlotte.ouryb@gmail.com>
2020-11-24 11:41:31 +0100

    start js

---
+++ js/dom.js
@@ -0,0 +1,12 @@
+const sendBtn = document.querySelector("#button");
+const textInput = document.querySelector("#words");
+
+function addText() {}
+
+function hideText() {}
+
+function changeFont() {}
+
+function changeButton() {}
+
+function revealText() {}
