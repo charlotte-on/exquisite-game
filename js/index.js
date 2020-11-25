8ad8b6387d7e27ee64885db04122b728b9de7f32
Charlotte Oury <charlotte.ouryb@gmail.com>
2020-11-25 19:09:35 +0100

    Game mostly done

--- js/index.js
+++ js/index.js
@@ -3,35 +3,84 @@ import { initEmojis } from "./emoji.js";
 const sendBtn = document.querySelector(".send");
 const textInput = document.querySelector("#words");
 const textWritten = document.querySelector("#written-text");
+const emojiBtn = document.querySelector(".trigger");
 
 initEmojis();
 
+let functionCalls = 0;
+
 function sendText() {
-  return (textWritten.textContent += textInput.value);
+  if (textWritten.textContent === "") {
+    textWritten.textContent += textInput.value;
+    hideText();
+    let start = revealLastWord();
+    functionCalls++;
+    return (textInput.value = `${start}`);
+  } else {
+    let string = textInput.value.split(" ");
+    let firstWord = string.splice(0, 3);
+    let withoutFirstWord = string.join(" ");
+    textWritten.textContent += ` ${withoutFirstWord}`;
+    hideText();
+    let start = revealLastWord();
+    functionCalls++;
+    console.log(functionCalls);
+    if (functionCalls === 9) {
+      changeButton();
+      return (textInput.value = `${start}`);
+    } else if (functionCalls === 10) {
+      removeInputs();
+      return revealText();
+    } else {
+      return (textInput.value = `${start}`);
+    }
+  }
 }
 
-// function checkInput() {
-//   var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-//   if (textInput.value.contains(regex)) {
-//     console.log("yeeeaaahh I'm here");
-//   }
-// }
+function hideText() {
+  textWritten.classList.add("hidden-text");
+}
 
-// function removeEmojis(string) {
-//   var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-//   console.log("yes");
-//   return string.replace(regex, "");
-// }
+function revealLastWord() {
+  let string = textInput.value.split(" ");
+  let lastWords =
+    string[string.length - 3] +
+    " " +
+    string[string.length - 2] +
+    " " +
+    string[string.length - 1] +
+    " ";
+  return lastWords;
+}
 
-function hideText() {}
+function changeButton() {
+  sendBtn.value = "Reveal";
+}
 
-function changeFont() {}
+function removeInputs() {
+  sendBtn.remove();
+  textInput.remove();
+  emojiBtn.remove();
+  // sendBtn.onclick = location.reload();
+}
 
-function changeButton() {}
-
-function revealText() {}
+function revealText() {
+  textWritten.classList.toggle("hidden-text");
+  textInput.value = "";
+  textInput.disabled = true;
+  sendBtn.disabled = true;
+}
 
 sendBtn.onclick = sendText;
+
+textInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendBtn.click();
+  }
+});
+
+// revealBtn.onclick = revealText;
 
 // textInput.onkeydown = () => {
 //   removeEmojis(textInput.value);
